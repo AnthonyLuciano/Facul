27eec93c036083eeb2e69b0e744f7f3a97e91fd4
mrmedicmain <102555390+mrmedicmain@users.noreply.github.com>
2024-05-09 16:20:40 -0300

    Add files via upload

---
+++ projetos_pessoais/Divisão_de_premio.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Divisão_de_premio {
+    public static void main(String[] args) {
+        //ler a entrada dos valores
+        Scanner ler = new Scanner(System.in);
+        System.out.print("Digite o valor do premio: ");
+        double premio = ler.nextDouble();
+        ler.close();
+        // calculo das porcentagens
+        double primeiro = premio * 0.46;
+        double segundo = premio * 0.32;
+        double terceiro = premio - (primeiro + segundo);
+        //impressão da quantidade dos es
+        System.out.println("Primeiro ganhador: R$ " + primeiro);
+        System.out.println("Segundo ganhador: R$ " + segundo);
+        System.out.println("Terceiro ganhador: R$ " + terceiro);
+    }
+}
