abba211897ffd86a594e29cdda0f763ffd38492c
Anthony Luciano <102555390+AnthonyLuciano@users.noreply.github.com>
2025-03-31 11:17:52 -0300

    removed useless "Console" calls

--- Analise e desenvolvimento de sistema/Gestão e qualidade/Geometria/Program.cs
+++ Analise e desenvolvimento de sistema/Gestão e qualidade/Geometria/Program.cs
@@ -9,15 +9,15 @@
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Digite o valor de X:");
-            double x = Convert.ToDouble(Console.ReadLine());
+            WriteLine("Digite o valor de X:");
+            double x = Convert.ToDouble(ReadLine());
 
-            Console.WriteLine("Digite o valor de Y:");
-            double y = Convert.ToDouble(Console.ReadLine());
+            WriteLine("Digite o valor de Y:");
+            double y = Convert.ToDouble(ReadLine());
 
             double z = 0;
-            Console.WriteLine("digite o valor de Z para o triangulo:");
-            z = Convert.ToDouble(Console.ReadLine());
+            WriteLine("digite o valor de Z para o triangulo:");
+            z = Convert.ToDouble(ReadLine());
 
             var retangulo = new Retangulo(x, y);
             var quadrado = new Quadrado(x);
@@ -25,12 +25,12 @@
             var circulo = new Circulo(x);
             var losango = new Losango(x, y);
 
-            Console.WriteLine("\n--- Cálculos ---");
-            Console.WriteLine($"Retângulo: Área = {retangulo.CalcularArea()}, Perímetro = {retangulo.CalcularPerimetro()}");
-            Console.WriteLine($"Quadrado: Área = {quadrado.CalcularArea()}, Perímetro = {quadrado.CalcularPerimetro()}");
-            Console.WriteLine($"Triângulo: Área = {triangulo.CalcularArea()}, Perímetro = {triangulo.CalcularPerimetro()}");
-            Console.WriteLine($"Círculo: Área = {circulo.CalcularArea()}, Perímetro = {circulo.CalcularPerimetro()}");
-            Console.WriteLine($"Losango: Área = {losango.CalcularArea()}, Perímetro = {losango.CalcularPerimetro()}");
+            WriteLine("\n--- Cálculos ---");
+            WriteLine($"Retângulo: Área = {retangulo.CalcularArea()}, Perímetro = {retangulo.CalcularPerimetro()}");
+            WriteLine($"Quadrado: Área = {quadrado.CalcularArea()}, Perímetro = {quadrado.CalcularPerimetro()}");
+            WriteLine($"Triângulo: Área = {triangulo.CalcularArea()}, Perímetro = {triangulo.CalcularPerimetro()}");
+            WriteLine($"Círculo: Área = {circulo.CalcularArea()}, Perímetro = {circulo.CalcularPerimetro()}");
+            WriteLine($"Losango: Área = {losango.CalcularArea()}, Perímetro = {losango.CalcularPerimetro()}");
 
         }
 
