cb96ece3eacbe372ca1538436f4b2e62fd9f2ac4
Anthony Luciano <102555390+AnthonyLuciano@users.noreply.github.com>
2025-03-31 11:03:59 -0300

    projeto Geometria C#

---
+++ Analise e desenvolvimento de sistema/Gestão e qualidade/Geometria/FormaGeometrica.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace CalculadoraDeArea
+{
+    public abstract class FiguraGeometrica
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public FiguraGeometrica(double x, double y, double z = 0)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public abstract double CalcularArea();
+        public abstract double CalcularPerimetro();
+    }
+
+    // Subclasse para Retângulo
+    public class Retangulo : FiguraGeometrica
+    {
+        public Retangulo(double x, double y) : base(x, y) { }
+
+        public override double CalcularArea()
+        {
+            return X * Y;
+        }
+
+        public override double CalcularPerimetro()
+        {
+            return 2 * (X + Y);
+        }
+    }
+
+    // Subclasse para Quadrado
+    public class Quadrado : FiguraGeometrica
+    {
+        public Quadrado(double x) : base(x, 0) { }
+
+        public override double CalcularArea()
+        {
+            return X * X;
+        }
+
+        public override double CalcularPerimetro()
+        {
+            return 4 * X;
+        }
+    }
+
+    // Subclasse para Triângulo
+    public class Triangulo : FiguraGeometrica
+    {
+        public Triangulo(double x, double y, double z) : base(x, y, z) { }
+
+        public override double CalcularArea()
+        {
+            return (X * Y) / 2;
+        }
+
+        public override double CalcularPerimetro()
+        {
+            return X + Y + Z;
+        }
+    }
+
+    // Subclasse para Círculo
+    public class Circulo : FiguraGeometrica
+    {
+        public Circulo(double x) : base(x, 0) { }
+
+        public override double CalcularArea()
+        {
+            return Math.PI * (X * X);
+        }
+
+        public override double CalcularPerimetro()
+        {
+            return 2 * Math.PI * X;
+        }
+    }
+
+    // Subclasse para Losango
+    public class Losango : FiguraGeometrica
+    {
+        public Losango(double x, double y) : base(x, y) { }
+
+        public override double CalcularArea()
+        {
+            return (X * Y) / 2;
+        }
+
+        public override double CalcularPerimetro()
+        {
+            double lado = Math.Sqrt(Math.Pow(X / 2, 2) + Math.Pow(Y / 2, 2));
+            return 4 * lado;
+        }
+    }
+}
---
+++ Analise e desenvolvimento de sistema/Gestão e qualidade/Geometria/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Geometria
+{
+    using CalculadoraDeArea;
+    using System.Security.AccessControl;
+    using static System.Console;
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite o valor de X:");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor de Y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double z = 0;
+            Console.WriteLine("digite o valor de Z para o triangulo:");
+            z = Convert.ToDouble(Console.ReadLine());
+
+            var retangulo = new Retangulo(x, y);
+            var quadrado = new Quadrado(x);
+            var triangulo = new Triangulo(x, y, z);
+            var circulo = new Circulo(x);
+            var losango = new Losango(x, y);
+
+            Console.WriteLine("\n--- Cálculos ---");
+            Console.WriteLine($"Retângulo: Área = {retangulo.CalcularArea()}, Perímetro = {retangulo.CalcularPerimetro()}");
+            Console.WriteLine($"Quadrado: Área = {quadrado.CalcularArea()}, Perímetro = {quadrado.CalcularPerimetro()}");
+            Console.WriteLine($"Triângulo: Área = {triangulo.CalcularArea()}, Perímetro = {triangulo.CalcularPerimetro()}");
+            Console.WriteLine($"Círculo: Área = {circulo.CalcularArea()}, Perímetro = {circulo.CalcularPerimetro()}");
+            Console.WriteLine($"Losango: Área = {losango.CalcularArea()}, Perímetro = {losango.CalcularPerimetro()}");
+
+        }
+
+
+    }
+}
