ed0d8f23a5a65bff477850924af9cb5200f04e7f
Anthony Luciano <102555390+AnthonyLuciano@users.noreply.github.com>
2025-04-08 11:04:48 -0300

    treino de POO no C#

---
+++ Analise e desenvolvimento de sistema/Gestão e qualidade/Geometria/Projeto POO/Metodos.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Metodos
+    {
+        public static void atividade1()
+        {
+            //atividade 1
+            WriteLine("==========atividade 1=========");
+            WriteLine("Digite um numero inteiro: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Write("o numero que voce digitou foi: " + x + "\n");
+        }
+        public static void atividade2() {
+            //atividade 2
+            WriteLine("==========atividade 2=============");
+            WriteLine("Digite um numero inteiro para somar: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            WriteLine("Digite outro numero inteiro para somar: ");
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+            int numero3 = numero + numero2;
+            WriteLine("Resposta: " + numero3 + "\n");
+        }
+        public static void atividade3() {
+            WriteLine("==========atividade 3=============");
+            WriteLine("digite um numero inteiro: ");
+            double numero4 = Convert.ToDouble(Console.ReadLine());
+            numero4 = Convert.ToInt32((int)numero4);
+            WriteLine(numero4 + "\n");
+        }
+        public static void atividade4() {
+            //atividade 4
+            WriteLine("==========atividade 4=============");
+            WriteLine("digame seu nome:");
+            string nome = ReadLine();
+            WriteLine("bem vindo: " + nome + "\n");
+        }
+        public static void atividade5() {
+            //atividade 5
+            WriteLine("==========atividade 5=============");
+            WriteLine("Digite um numero para a area:");
+            int area = Convert.ToInt32(Console.ReadLine());
+            WriteLine("Digite um numero para a altura");
+            int altura = Convert.ToInt32(Console.ReadLine());
+            int soma = area + altura;
+            WriteLine("o total do retangulo deu: " + soma);
+        }
+        public static void atividade6() {
+               
+        } 
+        public static void atividade7() { }
+        public static void atividade8() { }
+        public static void atividade9() { }
+        public static void atividade10() { }
+    }
+}
---
+++ Analise e desenvolvimento de sistema/Gestão e qualidade/Geometria/Projeto POO/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleApp3
+{
+    using static System.Console;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine(" 1.  entrada e saida de dados \n 2.  operação aditiva \n 3.  conversão de tipos \n 4.  concatenação de strings \n 5.  calculo de área\n 6.  uso if \n 7.  uso switch \n 8.  uso for \n 9.  uso while \n 10. uso do-while");
+            WriteLine("O que voce quer fazer: ");
+            int opção = Convert.ToInt32(Console.ReadLine());
+            switch (opção)
+            {
+                case 1:
+                    Metodos.atividade1();
+                    break;
+                case 2:
+                    Metodos.atividade2();
+                    break;
+                case 3:
+                    Metodos.atividade3();
+                    break;
+                case 4:
+                    Metodos.atividade4();
+                    break;
+                case 5:
+                    Metodos.atividade5();
+                    break;
+                default:
+                    WriteLine("Opção invalida");
+                    break;
+            }
+              
+            
+            
+        }
+    }
+}
